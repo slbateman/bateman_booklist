db95c95864f199a7c760493001c6acf93e29d5f1
Steve Bateman <steve@steves-mac-mini.lan>
2022-01-04 11:21:48 -0700

    global state complete

--- bateman_booklist/src/index.js
+++ bateman_booklist/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store/store"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
